756ad722c2359df13a952e4349d731095b6ff417
Rupam Kairi <rupamkairi@hotmail.com>
2023-10-01 18:04:28 +0530

    initial commit

---
+++ saaschat-app/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
