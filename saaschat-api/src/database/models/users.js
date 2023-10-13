55f1fb59d6528748475d235a53d07d2613588665
Rupam Kairi <rupamkairi@hotmail.com>
2023-10-13 15:09:16 +0530

    Database CRUD

--- saaschat-api/src/database/models/users.js
+++ saaschat-api/src/database/models/users.js
@@ -1,13 +1,62 @@
+import { v4 } from "uuid";
 import { client } from "../cassandra.js";
 
-export async function getUsers() {
+export async function createUser() {
   try {
-    const query = "SELECT * FROM users";
-    const result = await client.execute(query, []);
-    console.log("User with", result.rows);
+    const query = "";
+    let result = await client.execute(query, []);
+    result = result.rows.flat();
     return result;
   } catch (error) {
-    console.log("getUsers", error);
+    console.log("createUser", error);
+    client.shutdown();
+  }
+}
+
+export async function findUsers() {
+  try {
+    const query = "SELECT * FROM users";
+    let result = await client.execute(query, []);
+    result = result.rows.flat();
+    return result;
+  } catch (error) {
+    console.log("findUsers", error);
+    client.shutdown();
+  }
+}
+
+export async function findUserById(id) {
+  try {
+    const query = "SELECT * FROM users WHERE id = ?";
+    let result = await client.execute(query, [id]);
+    result = result.rows.flat();
+    return result;
+  } catch (error) {
+    console.log("findUserById", error);
+    client.shutdown();
+  }
+}
+
+export async function updateUserById(id) {
+  try {
+    const query = "UPDATE users SET updated_at = ? WHERE id = ?";
+    let result = await client.execute(query, [new Date(), id]);
+    result = result.rows;
+    return result;
+  } catch (error) {
+    console.log("updateUserById", error);
+    client.shutdown();
+  }
+}
+
+export async function deleteUserById(id) {
+  try {
+    const query = "DELETE FROM users WHERE id = ? IF EXISTS";
+    let result = await client.execute(query, [id]);
+    result = result.rows.flat();
+    return result;
+  } catch (error) {
+    console.log("deleteUserById", error);
     client.shutdown();
   }
 }
--- saaschat-api/src/websocket/onmessage.js
+++ saaschat-api/src/websocket/onmessage.js
@@ -1,4 +1,9 @@
-import { getUsers } from "../database/models/users.js";
+import {
+  findUsers,
+  findUserById,
+  updateUserById,
+  deleteUserById,
+} from "../database/models/users.js";
 import { actions } from "../utils/actions.js";
 import { widgetConnect } from "./handlers/open.js";
 
@@ -15,8 +20,7 @@ export async function onmessage(connection, event) {
 
   let result;
 
-  const users = await getUsers();
-  console.log(users, "-> db users");
+  console.log(await deleteUserById());
 
   if (segments[0] == actions.connect) {
     switch (segments[1]) {
