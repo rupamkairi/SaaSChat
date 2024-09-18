acb0a93e3003b007e38e81197c8e6f0cce7fad43
Rupam Kairi <rupamkairi@hotmail.com>
2024-09-18 12:03:28 +0530

    updated packages

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -4,14 +4,13 @@ export default defineConfig({
   schema: "./src/schema/*",
   out: "./src/drizzle",
   dialect: "postgresql",
-
   dbCredentials: {
-    // url: process.env.DATABASE_URL!,
-    host: process.env.DB_HOST!,
-    port: +process.env.DB_PORT!,
-    user: process.env.DB_USER!,
-    password: process.env.DB_PASS!,
-    database: process.env.DB_NAME!,
+    url: process.env.DATABASE_URL!,
+    // host: process.env.DB_HOST!,
+    // port: +process.env.DB_PORT!,
+    // user: process.env.DB_USER!,
+    // password: process.env.DB_PASS!,
+    // database: process.env.DB_NAME!,
     ssl: "require",
   },
 });
